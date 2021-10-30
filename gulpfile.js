69f60a776f29088c9195992ef8c65c8ca537e798
luischavez23 <lchavezdiazdesandi@gmail.com>
2021-10-30 18:58:09 -0500

    First commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,11 @@
 const {src, dest, watch, series} = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 
+//Images
+const image = require('gulp-imagemin')
+const webp = require('gulp-webp')
+const avif = require('gulp-avif')
+
 function css(done){
     //SASS compile
     //1. Identify files {src} = gulp  2.Compile sass 3.Save files {dest}
@@ -11,11 +16,36 @@ function css(done){
     done();
 }
 
+function images(){
+    return src('src/img/**/*')
+    .pipe( image({optimizationLevel: 3}) )
+    .pipe( dest('build/img') )
+}
+
+function versionWebp(){
+    return src('src/img/**/*.{jpg,png}')
+    .pipe(webp())
+    .pipe(dest('build/img'))
+}
+
+function versionAvif(){
+    const options = {
+        quality: 50
+    }
+    return src('src/img/**/*.{jpg,png}')
+    .pipe(avif( options ))
+    .pipe(dest('build/img'))
+}
+
 function dev(){
     watch('src/scss/**/*.scss', css);
+    watch('src/img/**/*', images)
 }
 
 
 exports.css = css;
 exports.dev = dev;
+exports.images = images;
+exports.versionWebp = versionWebp;
+exports.versionAvif = versionAvif
 exports.default = series( css, dev );
